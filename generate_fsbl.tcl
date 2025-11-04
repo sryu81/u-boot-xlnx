8842c9bc71f65c089be1b32e28dd957c994740b8
Soo Ryu <soo.ryu@lbl.gov>
2025-11-03 17:29:43 -0800

    fsbl tweak to enable qspi flash boot

--- generate_fsbl.tcl
+++ generate_fsbl.tcl
@@ -8,10 +8,30 @@ set platform_name [lindex $argv 0]
 set xsa_file [lindex $argv 1]
 set workspace_dir "./${platform_name}"
 
-setws ./$platform_name
-puts "INFO: Creating FSBL application..."
-app create -name fsbl -platform $platform_name -hw $xsa_file -proc ps7_cortexa9_0 -lang c -template {Empty Application}
+puts "INFO: Setting workspace to: $workspace_dir"
+setws $workspace_dir
+
+puts "INFO: Creating platform '$platform_name'..."
+platform create -name $platform_name -hw $xsa_file
+
+puts "INFO: Setting platform '$platform_name' as active..."
+platform active $platform_name
+
+puts "INFO: Explicitly creating the standalone software domain..."
+domain create -name {standalone_ps7_cortexa9_0} -os {standalone} -proc {ps7_cortexa9_0}
+
+puts "INFO: (First) Generating platform to create the base BSP..."
+platform generate
+
+puts "INFO: Adding xilffs library to standalone domain (FSBL dependency)..."
+domain active {standalone_ps7_cortexa9_0}
 bsp setlib -name xilffs
+
+puts "INFO: (Second) Regenerating platform to commit library change..."
+platform generate
+
+puts "INFO: Creating FSBL application..."
+app create -name fsbl -platform $platform_name -domain standalone_ps7_cortexa9_0 -template {Zynq FSBL}
 
 puts "INFO: Configuring FSBL Application Compiler Settings for QSPI..."
 app config -name fsbl -add define "FSBL_SD_EXCLUDE=1"
@@ -19,8 +39,6 @@ app config -name fsbl -add define "FSBL_DEBUG_INFO=1"
 
 puts "INFO: Building the FSBL application..."
 app build -name fsbl
-#platform generate
 
-file copy -force ./$platform_name/System/zynq_fsbl/fsbl.elf ./fsbl_sf.elf
 puts "INFO: Script finished successfully."
-puts "INFO: Your QSPI-configured 'fsbl_sf.elf'"
+puts "INFO: Your QSPI-configured 'fsbl.elf' is ready in '$workspace_dir/fsbl/Debug/'"
